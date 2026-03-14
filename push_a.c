f88b1580a48b926173f6d07050250f807a46cc77
Morgane-Lin Duhoux <mduhoux@shi-c1-p3.s19.be>
2026-03-14 18:07:16 +0100

    Ajout du push a

---
+++ push_a.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_a.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mduhoux <mduhoux@student.42belgium.be      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 11:43:24 by mduhoux           #+#    #+#             */
+/*   Updated: 2026/03/14 12:48:45 by mduhoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*void	ft_push(t_list **stack_a, **stack_b)
+{
+	
+}
+*/
+int	main(int ac, char **ag)
+{
+	if (ac < 2)
+		return (0);
+	t_list	*stack_a;
+	t_list	*stack_b;
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
+
+	stack_a = create_node(ft_atoi(ag[1]));
+	node1 = create_node(ft_atoi(ag[2]));
+	node2 = create_node(ft_atoi(ag[3]));
+	stack_b = create_node(ft_atoi(ag[4]));
+	node3 = create_node(ft_atoi(ag[5]));
+	add_list(&stack_a, node1);
+	add_list(&stack_a, node2);
+	add_list(&stack_b, node3);
+	printf("stack A\n");
+	while (stack_a != NULL)
+	{
+		printf("%d\n", stack_a->value);
+		stack_a = stack_a->next;
+	}
+	printf("stack B\n");
+	while (stack_b != NULL)
+	{
+		printf("%d\n", stack_b->value);
+		stack_b = stack_b->next;
+	}
+}
